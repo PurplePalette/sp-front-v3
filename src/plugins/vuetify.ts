efc8a6d7c20567e57f1bb254d05933e612f67762
Dosugamea <dsgamer777@gmail.com>
2022-05-04 20:43:56 +0900

    ➕ Add vuetify3

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,32 @@
+import { defineNuxtPlugin } from '#app'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi-svg'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin((nuxtApp) => {
+   const vuetify = createVuetify({
+      components,
+      directives,
+      icons: {
+         defaultSet: 'mdi',
+         aliases,
+         sets: {
+           mdi,
+         },
+      },
+      theme: {
+         defaultTheme: 'custom',
+         themes: {
+            custom: {
+               colors: {
+                  primary: '#5E35B1',
+                  secondary: '#7E57C2',
+               }
+            }
+         }
+      }
+   })
+   nuxtApp.vueApp.use(vuetify)
+})
