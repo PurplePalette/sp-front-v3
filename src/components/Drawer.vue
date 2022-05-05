154cd17a92aa37298642d289748bf2a1f580fa68
Dosugamea <dsgamer777@gmail.com>
2022-05-05 20:51:50 +0900

    🎨 Remove unneeded v-list-item-content wrapper

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -10,12 +10,10 @@
         v-if="auth.isLoggedIn"
         href="/profile/account"
       >
-        <v-list-item-content>
-          <v-icon aria-label="My Account" role="img" aria-hidden="false">
-            {{ mdiAccount }}
-          </v-icon>
-          <v-list-item-title>ユーザーページ</v-list-item-title>
-        </v-list-item-content>
+        <v-icon aria-label="My Account" role="img" aria-hidden="false">
+          {{ mdiAccount }}
+        </v-icon>
+        <v-list-item-title>ユーザーページ</v-list-item-title>
       </v-list-item>
       <v-list-item
         v-if="!auth.isLoggedIn"
@@ -24,9 +22,7 @@
         <v-list-item-icon>
           {{ mdiLogin }}
         </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>ログイン</v-list-item-title>
-        </v-list-item-content>
+        <v-list-item-title>ログイン</v-list-item-title>
       </v-list-item>
       <v-list-item
         v-for="(item,i) in nav"
@@ -37,9 +33,7 @@
         <v-list-item-icon>
           {{ item.icon }}
         </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>{{ $t(item.text) }}</v-list-item-title>
-        </v-list-item-content>
+        <v-list-item-title>{{ $t(item.text) }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
