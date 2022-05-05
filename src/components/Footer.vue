54ebd0d2b1718d3644a71750fa8440d5e7b13e52
Dosugamea <dsgamer777@gmail.com>
2022-05-05 20:57:26 +0900

    🎨 Reformat Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,37 +4,37 @@
     style="background-color: #7E57C2"
   >
     <v-container>
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      <v-col
-        v-for="(link, i) in links"
-        :key="i"
-        class="text-center"
-        cols="12" sm="12" md="6" lg="3"
+      <v-row
+        justify="center"
+        no-gutters
       >
-        <v-btn
-          class="mb-2"
-          variant="text"
+        <v-col
+          v-for="(link, i) in links"
+          :key="i"
+          class="text-center"
+          cols="12" sm="12" md="6" lg="3"
         >
-          <v-icon>{{ link.icon }}</v-icon>
-          <a style="text-decoration: none;" class="text-white" :href="link.to">{{ $t(link.text) }}</a>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row class="text-center" justify="center" align="center">
-      <v-col cols="12" sm="12" md="4" lg="6">
-        <small>
-          <strong>{{ new Date().getFullYear() }} — <strong>{{ config.OWNER_NAME }}</strong></strong>
-        </small>
-      </v-col>
-      <v-col class="my-1" cols="12" sm="12" md="8" lg="6">
-        <small class="pt-0">
-            <p>{{ $t("SweetPotato.Navigation.Footer.Notice") }}</p>
-        </small>
-      </v-col>
-    </v-row>
+          <v-btn
+            class="mb-2"
+            variant="text"
+          >
+            <v-icon>{{ link.icon }}</v-icon>
+            <a style="text-decoration: none;" class="text-white" :href="link.to">{{ $t(link.text) }}</a>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row class="text-center" justify="center" align="center">
+        <v-col cols="12" sm="12" md="4" lg="6">
+          <small>
+            <strong>{{ new Date().getFullYear() }} — <strong>{{ config.OWNER_NAME }}</strong></strong>
+          </small>
+        </v-col>
+        <v-col class="my-1" cols="12" sm="12" md="8" lg="6">
+          <small class="pt-0">
+              <p>{{ $t("SweetPotato.Navigation.Footer.Notice") }}</p>
+          </small>
+        </v-col>
+      </v-row>
     </v-container>
   </footer>
 </template>
