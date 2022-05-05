4029790814175996c3a4a78b392766318084193e
Dosugamea <dsgamer777@gmail.com>
2022-05-06 02:14:28 +0900

    🚧 🎨 Implement SearchFormLevel

--- src/pages/explorer/levels.vue
+++ src/pages/explorer/levels.vue
@@ -1,10 +1,22 @@
 <template>
   <v-container>
-    <ListBox :jump="true" :tab-index="0">
-      <v-window-item :value="0">
-        aaa
-      </v-window-item>
-    </ListBox>
+    <v-row justify="center">
+      <v-col>
+        <ListBox :jump="true" :tab-index="0">
+          <!-- 検索フォーム -->
+          <v-window-item :value="0">
+            <SearchFormLevel />
+          </v-window-item>
+        </ListBox>
+      </v-col>
+    </v-row>
+    <v-row class="mt-2" justify="center">
+      <v-col>
+        <v-card>
+          検索結果表示欄
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
