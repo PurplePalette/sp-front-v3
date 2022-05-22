12bd56f9dcbbd461d95e5aa05c5c5f90214b890f
Dosugamea <dsgamer777@gmail.com>
2022-05-22 19:42:43 +0900

    ✨ Add level upload form handler and keep input

--- src/pages/upload/levels.vue
+++ src/pages/upload/levels.vue
@@ -1,14 +1,47 @@
 <template>
     <UploadFormTemplate :stepId="stepId - 1">
-      <UploadFormLevelFile v-if="stepId == 2" />
-      <UploadFormLevelText v-if="stepId == 3" />
-      <UploadFormLevelConfirm v-if="stepId == 4" />
+      <UploadFormLevelFile
+        v-if="stepId == 2"
+        v-model:jacketTitle="jacketTitle"
+        v-model:musicTitle="musicTitle"
+        v-model:levelTitle="levelTitle"
+        @update:jacket="(file) => { jacket = file }"
+        @update:music="(file) => { music = file }"
+        @update:level="(file) => { level = file }"
+        @update:filled="() => { disableNext = false }"
+      />
+      <UploadFormLevelText
+        v-if="stepId == 3"
+        v-model:title="title"
+        v-model:description="description"
+        v-model:artist="artist"
+        v-model:author="author"
+        v-model:genre="genre"
+        v-model:engine="engine"
+        v-model:difficulty="difficulty"
+        @update:filled="() => { disableNext = false }"
+        @update:unfilled="() => { disableNext = true }"
+      />
+      <UploadFormLevelConfirm
+        v-if="stepId == 4"
+        :title="title"
+        :description="description"
+        :artist="artist"
+        :author="author"
+        :genre="genre"
+        :difficulty="difficulty"
+        :jacket="jacket"
+        :music="music"
+        :level="level"
+        @update:confirmed="upload"
+      />
       <UploadFormFinish v-if="stepId == 5" />
       <UploadFormNextPrev
+        v-if="stepId != 5"
         :stepId="stepId"
-        :disablePrev="stepId == 2"
-        @update:next="stepId += 1"
-        @update:prev="stepId -= 1"
+        :disableNext="disableNext"
+        @update:next="stepId += 1; disableNext = stepId <= 3"
+        @update:prev="stepId -= 1; disableNext = false;"
       />
     </UploadFormTemplate>
 </template>
@@ -17,32 +50,32 @@
 useTitle("Upload")
 
 const stepId = ref<number>(2)
+const disableNext = ref<boolean>(true)
+
 // Step2
-const jacketFile = ref<File | null>(null)
-const musicFile = ref<File|null>(null)
-const levelFile = ref<File|null>(null)
-const goStep3 = computed(() => {
-  return (jacketFile != null && musicFile != null && levelFile != null)
+const jacket = ref<File | null>(null)
+const music = ref<File|null>(null)
+const level = ref<File|null>(null)
+
+const jacketTitle = computed(() => {
+    return jacket.value ? jacket.value.name : "表紙(.jpg)を選択"
 })
+const musicTitle = computed(() => {
+    return music.value ? music.value.name : "音源(.mp3)を選択"
+})
+const levelTitle = computed(() => {
+    return level.value ? level.value.name : "譜面(.sus)を選択"
+})
+
 
 // Step3
 const title = ref<string>('')
 const description = ref<string>('')
 const artist = ref<string>('')
 const author = ref<string>('')
-const genre = ref<string>('')
-const engine = ref<string>('')
+const genre = ref<string>('GENERAL')
+const engine = ref<string>('SEKAI')
 const difficulty = ref<number>(1)
-const goStep4 = computed(() => {
-    return (
-        title.value.length > 0
-        && description.value.length > 0
-        && artist.value.length > 0
-        && author.value.length > 0
-        && genre.value.length > 0
-        && engine.value.length > 0
-    )
-})
 
 // Step5
 const upload = () => {
